d18a454bf35fbedd104a0fb5d39cd09bd8e88c76
Eliott Paris <eliottparis@gmail.com>
2019-05-21 17:06:40 +0200

    Add python test file

---
+++ test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+#     setup.py
+#     Copyright (c) 2019 Eliott PARIS
+#
+#Tab=3########################################################################
+
+
+##### IMPORT #################################################################
+
+from __future__ import print_function
+
+import sys
+
+##############################################################################
+
+class A (object):
+   def __init__ (self):
+      pass
+
+if sys.version_info < (2, 7):
+   print >> sys.stderr, 'This script requires python 2.7 or greater.'
+   sys.exit (1)
+
+def main ():
+   a = A ()
+   print ('python version:', sys.version)
+   print ('This script requires python 2.7 or greater.', file=sys.stderr)
+   print ('print')
+   print ('print')
+   line = 'printline'
+   print (line,)
+
+##############################################################################
+
+if __name__ == '__main__':
+   sys.exit (main ())
