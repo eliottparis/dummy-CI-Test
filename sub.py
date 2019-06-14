f9823babe72bf6d6e5b798a976fe9dc7822de29b
Eliott Paris <eliottparis@gmail.com>
2019-06-14 11:59:34 +0200

    update script and travis

---
+++ sub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+#     sub.py
+#     Copyright (c) 2019 Eliott PARIS
+#
+#Tab=3########################################################################
+
+
+##### IMPORT #################################################################
+
+from __future__ import print_function
+
+import sys
+
+##############################################################################
+
+if __name__ == '__main__':
+   print ('############')
+   print ('- sub => python version:', sys.version)
+   sys.exit (0)
--- test.py
+++ test.py
@@ -10,7 +10,13 @@
 
 from __future__ import print_function
 
+import os
+import subprocess
 import sys
+
+if sys.version_info < (2, 7):
+   print >> sys.stderr, 'This script requires python 2.7 or greater.'
+   sys.exit (1)
 
 ##############################################################################
 
@@ -18,18 +24,32 @@ class A (object):
    def __init__ (self):
       pass
 
-if sys.version_info < (2, 7):
-   print >> sys.stderr, 'This script requires python 2.7 or greater.'
-   sys.exit (1)
-
 def main ():
    a = A ()
-   print ('python version:', sys.version)
-   print ('This script requires python 2.7 or greater.', file=sys.stderr)
+   print ('- test.py => python version:', sys.version)
+
+   if 'TRAVIS' in os.environ:
+      print ('Running on Travis-CI')
+      if 'TRAVIS_PYTHON_VERSION' in os.environ:
+         print ('TRAVIS_PYTHON_VERSION:', os.environ ['TRAVIS_PYTHON_VERSION'])
+      else:
+         print ('TRAVIS_PYTHON_VERSION undefined')
+
+   returncode = 50
+   print ('test error', returncode, file=sys.stderr)
    print ('print')
-   print ('print')
+
+   print ('############')
    line = 'printline'
-   print (line, end='')
+   print (line, end=' ')
+   print ('end')
+
+   print ('############')
+   python_exe = sys.executable
+   print ('python_exe:', python_exe)
+
+   cmd = [python_exe, 'sub.py']
+   subprocess.check_call (cmd)
 
 ##############################################################################
 
